6ab0de3e7d0cc076c5151a3cd9e6f2fba8221a7c
AnalistSingidava <analistsingidava@gmail.com>
2023-10-10 22:10:38 +0300

    Update beta.js

--- orientare/beta/beta.js
+++ orientare/beta/beta.js
@@ -12,7 +12,11 @@ function on_device_orientation(evt) {
   var centru = { x: canvas.width / 2, y: canvas.height / 2 }
   var latura_patrat = 50;
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.fillStyle = "#6A0888";
+  if (beta < 0) {
+    context.fillStyle = "#6A0888";
+  } else {
+    context.fillStyle = "#A68088";
+  }
   context.beginPath();
-  context.fillRect(180, 150, evt.beta, 50);
+  context.fillRect(180, 150, beta, 50);
 }
