c7070b2d33def76920e039141624fc30b07427c1
AnalistSingidava <analistsingidava@gmail.com>
2023-10-10 22:37:38 +0300

    added 3rd square

--- orientare/beta/beta.js
+++ orientare/beta/beta.js
@@ -25,12 +25,25 @@ function on_device_orientation(evt) {
   var centru = { x: canvasgamma.width / 2, y: canvasgamma.height / 2 }
   var latura_patrat = 50;
   contextgamma.clearRect(0, 0, canvasgamma.width, canvasgamma.height);
-  if (beta < 0) {
+  if (gamma < 0) {
     contextgamma.fillStyle = "#6A0888";
   } else {
     contextgamma.fillStyle = "#FF8080";
   }
   contextgamma.beginPath();
   contextgamma.fillRect(180, 150, 50, gamma);
+
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  var centru = { x: canvas.width / 2, y: canvas.height / 2 }
+  var latura_patrat = 50;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  if (beta + gamma < 0) {
+    context.fillStyle = "#6A0888";
+  } else {
+    context.fillStyle = "#FF8080";
+  }
+  context.beginPath();
+  context.fillRect(180, 150, beta, gamma);
 
 }
